eb50a5c3a68dd41ca3c82a43784f3fb6343b1b93
Bruno Krebs <bruno.krebs@auth0.com>
2019-07-10 08:56:27 -0700

    cleaning for exercise;

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const crypto = require('crypto');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
-const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 const request = require('request-promise');
 const session = require('express-session');
 
@@ -16,13 +17,17 @@ let oidcProviderInfo;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+// Configure Passport to use Auth0
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(crypto.randomBytes(16).toString('hex')));
-app.use(session({
-  secret: crypto.randomBytes(32).toString('hex'),
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: crypto.randomBytes(32).toString('hex'),
+    resave: false,
+    saveUninitialized: false
+  })
+);
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
@@ -32,10 +37,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/profile', (req, res) => {
-  const {idToken, decodedIdToken} = req.session;
+  const { user } = req.session.passport;
   res.render('profile', {
-    idToken,
-    decodedIdToken,
+    idToken: user.idToken,
+    decodedIdToken: user._json
   });
 });
 
